f589f3c450f166950a5c63f3e8297cdc2c12a5a0
Juliane Bruck <120400449+Juliane2210@users.noreply.github.com>
2023-11-18 10:01:07 -0500

    Delete PythonSchool/Lab2/Lab 2 directory

--- PythonSchool/Lab2/Lab 2/testLabo2.py
+++
@@ -1,9 +0,0 @@
-def celsius_en_fahrenheit (temp_Celsius):
-    temp_Fahrenheit = (temp_Celsius * 9.0/5.0) + 32
-    return temp_Fahrenheit
-
-
-
-t_celsius = 100
-t_fahrenheit = celsius_en_fahrenheit(t_celsius) 
-print(t_celsius, "Celsius est", t_fahrenheit, "Fahrenheit." )
